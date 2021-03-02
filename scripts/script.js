7f134b61f629ee8af8b702d3e045660f88b7fffa
Douglas Amport <douglas.amport@gmail.com>
2021-03-02 17:32:57 -0500

    finish keyboard integration

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,162 @@
-//  TODO
-//
-//
-//backspace button
-//keyboard support  use keydown even
+//TODO
+//1. combine keydown events for active class & input.
 
 
 /*******************************/
 
-//arithmatic functions
 const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => Number(a) - Number(b);
 const multiply = (a, b) => Number(a) * Number(b);
 const divide = (a, b) => Number(a)/Number(b);
-// more than 2 arguments
-// function add() { return [...arguments].reduce((a, b) => a + b, 0);}
-// function multiply() {return [...arguments].reduce((a, b) => a * b, 1);}
+const operate = (func, a, b) => func(a, b)
 
-function operate(func, a, b) {
-    return func(a, b);
-}
-
-//working variables 
 let total = '';
 let workingValue = '';
 let valueA = '';
 let valueB = '';
 let operation = '';
-let sumbol = ''
+let symbol = ''
 
 const output = document.querySelector("#output");
 const summary = document.querySelector("#summary")
 
-//Keyboard support
+//Keyboard support for data entry
+startKeyboardNumberListener();
+startKeyboardFuncListener();
 
-const keydown = document.addEventListener("keydown", keydownHandler)
+document.addEventListener('keydown', keyboardToggleActive)
+document.addEventListener('keyup', keyboardToggleActive)
 
-function keydownHandler(e) {
-    console.log(e.key)
+function keyboardToggleActive(e) {
+    let thisValue;
+        switch (e.key) {
+            case '1':
+                thisValue = 'one';
+                break;
+            case '2':
+                thisValue = 'two';
+                break;
+            case '3':
+                thisValue = 'three';
+                break;
+            case '4':
+                thisValue = 'four';
+                break;
+            case '5':
+                thisValue = 'five';
+                break;
+            case '6':
+                thisValue = 'six';
+                break;
+            case '7':
+                thisValue = 'seven';
+                break;
+            case '8':
+                thisValue = 'eight';
+                break;
+            case '9':
+                thisValue = 'nine';
+                break;
+            case '0':
+                thisValue = 'zero';
+                break;
+            case '+':
+                thisValue = 'add';
+                break;
+            case '-':
+                thisValue = 'subtract';
+                break;
+            case '*':
+                thisValue = 'multiply';
+                break;
+            case '/':
+                    thisValue = 'divide';
+                    break;
+            case '=': 
+                thisValue = 'equals';
+                break;
+            case 'Enter':
+                thisValue = 'equals';
+                break;
+            case 'Backspace':
+                thisValue = 'del';
+                break;
+            default:
+                return;
+                break;
+    }
+    let item = document.querySelector(`#${thisValue}`);
+    item.classList.toggle('active');
 }
 
+function startKeyboardNumberListener() {
+document.addEventListener("keydown", keyDownNumHandler);
+};
 
+function keyDownNumHandler(e) {
+    if (e.key >= 0 || e.key<= 9) {
+        keyboardNumberInput(e.key)
+    }
+};
+
+function startKeyboardFuncListener () {
+document.addEventListener('keydown', keyDownFuncHandler);
+};
+
+function keyDownFuncHandler(e) {
+    let regEx = /\./;
+    if (e.key === '+') {
+        sumFunction();
+    }
+    else if (e.key === '-') {
+        differenceFunction();
+    }
+    else if (e.key === '*') {
+        productFunction();
+    }
+    else if (e.key === '/') {
+        quotientFunction();
+    }
+    else if (e.key === '=' || e.key === 'Enter') {
+        equalsFunction();
+    }
+    else if (e.key === 'Backspace') {
+        deleteFunction();
+    }
+    else if (!regEx.test(workingValue) && e.key === '.') {
+        
+        keyboardNumberInput(e.key);
+    }
+}
+
+function keyboardNumberInput(value) {
+    let regEx = /\./
+    workingValue += value
+    output.innerText = workingValue;
+    if (regEx.test(workingValue)){
+        decimal.removeEventListener("click", clickNumberInput)
+    }
+    if (workingValue.length >= 11) {
+        endNumberListeners()
+        endKeyboardNumberListener()   
+    }
+};
+
+function endKeyboardNumberListener() {
+    document.removeEventListener("keydown", keyDownNumHandler)
+}
 
 //number key variables and event listeners
 let allNumbers = document.querySelectorAll(".number-key")
 
 startNumberListeners()
+
+function startNumberListeners() {
+    for (let i = 0; i < allNumbers.length; i++) {
+        allNumbers[i].addEventListener("click", clickNumberInput);   
+    }
+};
+
 //declared num keys seperately to limit number length
 animateNumKeyOnTap()
 
@@ -54,17 +166,10 @@ function animateNumKeyOnTap() {
     }
 }
 
-function startNumberListeners() {
-    for (let i = 0; i < allNumbers.length; i++) {
-        allNumbers[i].addEventListener("click", numberButtonInput);   
-    }
-};
-
 //declared seperately to limit usage per number
 let decimal = document.querySelector("#decimal");
 decimal.addEventListener("click" , toggleActiveClass);
-decimal.addEventListener("click", numberButtonInput);
-
+decimal.addEventListener("click", clickNumberInput);
 
 //function Key Animation
 let allFuncKeys = document.querySelectorAll(".function-key")
@@ -77,77 +182,67 @@ function animateFuncKeyOnTap() {
     }
 }
 
-//function key 
-let sum = document.querySelector("#add");
-sum.addEventListener("click", function() {
+//function keys
+function clearPartial() {
     valueB = '';
     total = '';
     valueA = combineValues(valueA, workingValue);
-    symbol = '+';
     summary.innerText = `${valueA} ${symbol}`;
-    //output.innerText = formatDisplay(valueA);
     output.innerText = '';
-    operation = add;
+
+}
+function resetPartial() {
     workingValue = '';
-    decimal.addEventListener("click", numberButtonInput);
+    decimal.addEventListener("click", clickNumberInput);
     startNumberListeners()
-    // console.log(valueA);
-    // console.log(workingValue)
-});
+    startKeyboardNumberListener()
+
+}
+
+let sum = document.querySelector("#add");
+sum.addEventListener("click", sumFunction);
+
+function sumFunction() {
+    symbol = '+';
+    clearPartial();
+    operation = add;
+    resetPartial();
+};
 
 let difference = document.querySelector("#subtract");
-difference.addEventListener("click", function() {
-    valueB = '';
-    total = '';
-    valueA = combineValues(valueA, workingValue);
+difference.addEventListener("click", differenceFunction);
+
+function differenceFunction() {
     symbol = '-';
-    summary.innerText = `${valueA} ${symbol}`;
-    //output.innerText = formatDisplay(valueA);
-    output.innerText = '';
+    clearPartial();
     operation = subtract;
-    workingValue = '';
-    decimal.addEventListener("click", numberButtonInput);
-    startNumberListeners()
-    // console.log(valueA);
-    // console.log(workingValue)
-});
+    resetPartial();
+};
 
 let product = document.querySelector("#multiply");
-product.addEventListener("click", function() {
-    valueB = ''
-    total = ''
-    valueA = combineValues(valueA, workingValue);
-    symbol = 'x'
-    summary.innerText = `${valueA} ${symbol}`;
-    //output.innerText = formatDisplay(valueA);
-    output.innerText = '';
+product.addEventListener("click", productFunction);
+
+function productFunction() {
+    symbol = 'x';
+    clearPartial();
     operation = multiply;
-    workingValue = '';
-    decimal.addEventListener("click", numberButtonInput);
-    startNumberListeners()
-    // console.log(valueA);
-    // console.log(workingValue)
-});
+    resetPartial();
+};
 
 let quotient = document.querySelector("#divide");
-quotient.addEventListener("click", function() {
-    valueB = '';
-    total = '';
-    valueA = combineValues(valueA, workingValue);
-    symbol = '/'
-    summary.innerText = `${valueA} ${symbol}`;
-    //output.innerText = formatDisplay(valueA);
-    output.innerText = '';
+quotient.addEventListener("click", quotientFunction);
+
+function quotientFunction() {
+    symbol = '/';
+    clearPartial();
     operation = divide;
-    workingValue = '';
-    decimal.addEventListener("click", numberButtonInput);
-    startNumberListeners()
-    // console.log(valueA);
-    // console.log(workingValue)
-});
+    resetPartial();
+};
 
 let equals = document.querySelector("#equals");
-equals.addEventListener("click", function() {
+equals.addEventListener("click", equalsFunction)
+
+function equalsFunction() {
     if (valueB !== '') {
         valueB = valueB
     }
@@ -158,7 +253,8 @@ equals.addEventListener("click", function() {
     workingValue = '' ;
     valueA = total;
     endNumberListeners();
-});
+    endKeyboardNumberListener()
+};
 
 let clear = document.querySelector("#clear");
 clear.addEventListener("click", function() {
@@ -167,15 +263,16 @@ clear.addEventListener("click", function() {
     valueB = '';
     total = '';
     operation = '';
+    symbol = ''
     output.innerText = '0';
     summary.innerText = '';
-    decimal.addEventListener("click", numberButtonInput);
+    decimal.addEventListener("click", clickNumberInput);
     startNumberListeners();
+    startKeyboardNumberListener();
 });
 
 let sign = document.querySelector("#sign");
 sign.addEventListener("click", () => {
-    console.log(valueA, valueB, workingValue, total)
     if (valueA !== '' && valueA === total) {
         total = convertOppositeSign(total)
         valueA = convertOppositeSign(valueA);
@@ -185,8 +282,6 @@ sign.addEventListener("click", () => {
        workingValue =  convertOppositeSign(workingValue)
        output.innerValue = workingValue;
     }
-
-    
 });
 
 function convertOppositeSign(value) {
@@ -203,23 +298,27 @@ function convertOppositeSign(value) {
 }
 
 let del = document.querySelector("#del");
-del.addEventListener("click", () => {
+del.addEventListener("click", deleteFunction)
+
+function deleteFunction() {
     if (workingValue !== '') {
         workingValue = workingValue.slice(0, (workingValue.length-1));
         if (workingValue === '') {output.innerText = 0; return}
         output.innerText = workingValue;
     }
-});
+    startNumberListeners();
+    startKeyboardNumberListener();
+};
 
 
 
 //Number Button Function - turns off number at length 11
-function numberButtonInput() {
+function clickNumberInput() {
     regEx = /\./
-    workingValue += this.innerText;
+    workingValue += this.innerText
     output.innerText = workingValue;
     if (regEx.test(workingValue)){
-        decimal.removeEventListener("click", numberButtonInput)
+        decimal.removeEventListener("click", clickNumberInput)
     }
     if (workingValue.length >= 11) {
         endNumberListeners()
@@ -229,7 +328,7 @@ function numberButtonInput() {
 
 function endNumberListeners() {
     for (let i = 0; i < allNumbers.length; i++) {
-        allNumbers[i].removeEventListener("click", numberButtonInput)
+        allNumbers[i].removeEventListener("click", clickNumberInput)
     }
 }
 
@@ -237,9 +336,7 @@ function endNumberListeners() {
 function toggleActiveClass() {
     this.classList.add("active");
     setTimeout( () => {
-        //console.log(this.classList)
         this.classList.remove("active")
-        //console.log(this.classList)
     }, 75)
 
 };
@@ -247,25 +344,18 @@ function toggleActiveClass() {
 function formatDisplay(value) {
     if (value.toString(10).length > 11) {
         if (value > 99999999999 ) {
-            console.log(value)
-            console.log(value.toString(10).length)
-            let e = value.toString(10).length -1
-            console.log(e + 'check here')
-            let newValue = value / 10**(e)
-            console.log(newValue)
-            rounded = Math.trunc(newValue * 10**6) / 10**6
-            console.log(rounded)
-             return value = `${rounded}e${e}`
+            let e = value.toString(10).length -1;
+            let newValue = value / 10**(e);
+            rounded = Math.trunc(newValue * 10**6) / 10**6;
+             return value = `${rounded}e${e}`;
         }
         else {
-            let i = value.toString(10).search(/\./)
-            console.log(value + "this is where I print")
-            return value.toFixed(11 -i -1)
+            let i = value.toString(10).search(/\./);
+            return value.toFixed(11 -i -1);
         }    
     }
     else {return value}
 }
-
 
 function decimalCheckAndShift(a, w) {
     //a is for valueA and w is workingValue or valueB in the equals function
@@ -278,28 +368,25 @@ function decimalCheckAndShift(a, w) {
             else if (operation === multiply && w === '') {
                 w = 1;
             }
-            //get the minimum number of zeros needed to convert the decimals to integars
+//get the minimum number of zeros needed to convert the decimals to integars
             let arr = [a.toString(10), w.toString(10)]
                       .filter(a => regEx.test(a))
                       .map(a => a.length - a.search(regEx) -1);
-            let maxDecimal = Math.max(...arr);
-            
-            //convert both numbers to integars
+            let maxDecimal = Math.max(...arr); 
+//convert both numbers to integars
             a *= Math.pow(10, maxDecimal);
             w *= Math.pow(10, maxDecimal);
-            //perform the desired operation
+//perform the desired operation
             a = operate(operation, a, w);
-            //and return the decimal to the correct place
+//and return the decimal to the correct place
                 if (operation === divide) {
                     return a;
                 }
                 else if (operation === multiply) {
-                    console.log(a + ' just before conversion')
                     a = (a/(10**(2*maxDecimal)))
                     return a
                 }
                 else  {
-
                  a /= (10**maxDecimal);
                 }
             }
